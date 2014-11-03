b3d7879a46f1e04a6d89dcd544cbfc906413be25
anthony <anthony@iamfree.com>
2014-11-03 12:47:55 +1100

    repos

---
+++ Berksfile
@@ -0,0 +1,7 @@
+source "https://supermarket.getchef.com"
+
+metadata
+
+cookbook 'postgresql'
+cookbook 'imagemagick', '~> 0.2.3'
+cookbook 'wkhtmltopdf', '~> 0.2.0'
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'freedomserver'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures freedomserver'
+long_description 'Installs/Configures freedomserver'
+version          '0.1.0'
+
